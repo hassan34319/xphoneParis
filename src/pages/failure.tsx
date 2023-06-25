7677bec8d1fa31210770d756a129a3f345288534
hassan34319 <mh08062@st.habib.edu.pk>
2023-06-25 07:39:57 +0500

    New

--- src/pages/failure.tsx
+++ src/pages/failure.tsx
@@ -9,7 +9,7 @@ function Failure({}: Props) {
 
   // Example of how to unencrypt blowfish string
   return (
-    <div className="flex items-center justify-start mt-20 h-screen">
+    <div className="flex justify-center mt-20 h-screen">
       <h1 className="text-4xl font-bold text-red-500">Payment Failed</h1>
     </div>
   );
