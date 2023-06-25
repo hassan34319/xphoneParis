fb76bf01486e0903998a632935a871d7e5c863fb
hassan34319 <mh08062@st.habib.edu.pk>
2023-06-25 07:36:55 +0500

    Changed success and failure logic

--- src/pages/api/failure.tsx
+++ src/pages/api/failure.tsx
@@ -3,10 +3,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     // Perform any necessary processing or validation here
+    // Set cache-control headers to prevent caching
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
 
     // Redirect to the failure URL
     res.redirect(307, "https://xphones.fr/failure");
+    // Redirect to the failure URL
   } else {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+
     res.redirect(307, "https://xphones.fr/failure");
   }
 };
--- src/pages/api/success.tsx
+++ src/pages/api/success.tsx
@@ -3,10 +3,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     // Perform any necessary processing or validation here
-
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
     // Redirect to the failure URL
     res.redirect(307, "https://xphones.fr/success");
   } else {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
     res.redirect(307, "https://xphones.fr/success");
   }
 };
