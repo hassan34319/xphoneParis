978f2d3381451e18eca6799894ffafe661c741f5
hassan34319 <mh08062@st.habib.edu.pk>
2023-06-25 07:47:51 +0500

    New commit

--- src/pages/api/failure.tsx
+++ src/pages/api/failure.tsx
@@ -9,18 +9,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.setHeader("Expires", "0");
 
     // Redirect to the failure URL
-    res.writeHead(302, {
-        Location: "https://xphones.fr/failure",
-      });;
+    res.redirect(307, "https://xphones.fr/failure");
     // Redirect to the failure URL
   } else {
     res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", "0");
 
-    res.writeHead(302, {
-        Location: "https://xphones.fr/failure",
-      });;
+    res.redirect(307, "https://xphones.fr/failure");
   }
 };
 
