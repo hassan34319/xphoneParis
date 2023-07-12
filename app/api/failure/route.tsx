f420e56a3fd78f4dca494adbea170c17a83f09dd
hassan34319 <mh08062@st.habib.edu.pk>
2023-07-13 00:19:14 +0500

    Fixed route issues

--- app/api/failure/route.tsx
+++ app/api/failure/route.tsx
@@ -1,9 +1,9 @@
 import { NextApiRequest } from "next";
 import { redirect } from "next/navigation";
 
-export async function GET(request : NextApiRequest) {
-  redirect('/failure')
+export async function GET(request : Request) {
+  Response.redirect('/failure', 302)
 }
-export async function POST(request : NextApiRequest) {
-  redirect('/failure')
+export async function POST(request : Request) {
+  Response.redirect('/failure', 302)
 }
--- app/api/success/route.tsx
+++ app/api/success/route.tsx
@@ -1,9 +1,9 @@
 import { NextApiRequest } from "next";
 import { redirect } from "next/navigation";
 
-export async function GET(request : NextApiRequest) {
-  redirect('/success')
+export async function GET(request : Request) {
+  Response.redirect('/success', 302)
 }
-export async function POST(request : NextApiRequest) {
-  redirect('/success')
+export async function POST(request : Request) {
+  Response.redirect('/success', 302)
 }
