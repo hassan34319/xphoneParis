a8d4c0082e6211d3ae0185d2ae1f9d7712712dd6
hassan34319 <mh08062@st.habib.edu.pk>
2024-05-17 23:38:58 +0500

    Changes posted

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -32,7 +32,7 @@ async function InventoryPage({}: Props) {
   const results = await sanityClient.fetch(query);
 
   // Flatten the variants array
-  const variants = results.flatMap((product: Product) =>
+  const variants = results && results.flatMap((product: Product) =>
     product.variants.map((variant: Variant) => ({
       ...variant,
       productId: product._id,
@@ -56,7 +56,7 @@ async function InventoryPage({}: Props) {
           </tr>
         </thead>
         <tbody>
-          {variants.map((variant: Variant) => (
+          {variants && variants.map((variant: Variant) => (
             <tr
               key={`${variant.productId}-${variant.grade}-${variant.capacity}-${variant.color}`}
               className="hover:bg-gray-50"
