19af613d2006814b43ae89421b7bdb522180ab36
unknown <mh08062@st.habib.edu.pk>
2025-01-07 17:42:15 +0500

    Maintenance added

--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -1,4 +1,4 @@
-// pages/maintenance.js
+// pages/maintenance.tsx
 import React from 'react';
 import Head from 'next/head';
 
@@ -9,9 +9,9 @@ const Maintenance = () => {
         <title>Site Under Maintenance</title>
       </Head>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center p-4">
-        <h1 className="text-4xl font-semibold text-gray-800 mb-4">We'll be back soon!</h1>
+        <h1 className="text-4xl font-semibold text-gray-800 mb-4">We&apos;ll be back soon!</h1>
         <p className="text-xl text-gray-600 mb-6">
-          Sorry for the inconvenience but we're performing some maintenance at the moment. We'll
+          Sorry for the inconvenience but we&apos;re performing some maintenance at the moment. We&apos;ll
           be back online shortly!
         </p>
         <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
