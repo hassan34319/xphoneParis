17aca7316ce21efa313ba5d76f958b39462e7409
Ali IMran <si07886@st.habib.edu.pk>
2025-02-16 14:34:02 -0600

    newwsss

--- app/components/ProductReview.tsx
+++ app/components/ProductReview.tsx
@@ -290,7 +290,7 @@ function ProductReview({ id, currentUser, review: initialReviews }: Props) {
 <div className="relative">
   <label
     htmlFor="file-upload"
-    className="cursor-pointer bg-black text-white px-4 py-2 rounded-md text-sm font-semibold hover:bg-gray-800"
+    className="cursor-pointer bg-blue-400 text-white px-4 py-2 rounded-full text-sm font-semibold hover:bg-gray-800"
   >
     Upload Your Images
   </label>
