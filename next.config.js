9f3519832a6f4b27c5c9191f639bbad0046d8c9b
Ali IMran <si07886@st.habib.edu.pk>
2025-02-26 09:51:34 -0600

    new change

--- next.config.js
+++ next.config.js
@@ -11,17 +11,17 @@ const nextConfig = {
   images: {
     domains: ["flowbite.com", "image.noelshack.com", "www.noelshack.com", "res.cloudinary.com","cdn.sanity.io", "fiverr-res.cloudinary.com","www.backmarket.be","m.media-amazon.com","www.pngall.com","www.rueducommerce.fr"],
   },
-  redirects() {
-    return [
-      process.env.MAINTENANCE_MODE === "1"
-        ? {
-            source: "/((?!maintenance).*)",
-            destination: "/maintenance",
-            permanent: false,
-          }
-        : null,
-    ].filter(Boolean);
-  },
+  // redirects() {
+  //   return [
+  //     process.env.MAINTENANCE_MODE === "1"
+  //       ? {
+  //           source: "/((?!maintenance).*)",
+  //           destination: "/maintenance",
+  //           permanent: false,
+  //         }
+  //       : null,
+  //   ].filter(Boolean);
+  // },
  };
  
  module.exports = nextConfig;
