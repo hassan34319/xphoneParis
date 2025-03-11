834d5ec54567f72d754ae6d42dd39027a819cd5d
Ali IMran <si07886@st.habib.edu.pk>
2025-03-11 14:33:25 -0500

    new payment integrations

--- app/api/failure/route.tsx
+++ app/api/failure/route.tsx
@@ -2,8 +2,8 @@ import { NextApiRequest } from "next";
 import { redirect } from "next/navigation";
 
 export async function GET(request : Request) {
-  return Response.redirect('https://test-xphones.vercel.app/failure', 302)
+  return Response.redirect('https://xphones.fr/failure', 302)
 }
 export async function POST(request : Request) {
-  return Response.redirect('https://test-xphones.vercel.app/failure', 302)
+  return Response.redirect('https://xphones.fr/failure', 302)
 }
--- app/api/success/route.tsx
+++ app/api/success/route.tsx
@@ -2,8 +2,8 @@ import { NextApiRequest } from "next";
 import { redirect } from "next/navigation";
 
 export async function GET(request : Request) {
-  return Response.redirect('https://test-xphones.vercel.app/success', 302)
+  return Response.redirect('https://xphones.fr/success', 302)
 }
 export async function POST(request : Request) {
-  return Response.redirect('https://test-xphones.vercel.app/success', 302)
+  return Response.redirect('https://xphones.fr/success', 302)
 }
