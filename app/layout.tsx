931ba4f7188c0eca430990f91df7a8f370d851a7
Ali IMran <si07886@st.habib.edu.pk>
2025-05-16 20:02:41 -0500

    meta details added

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,19 @@ export default async function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/community.png" sizes="any" />
         
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WZC5VTKW');`
+          }}
+        />
+        
         {/* Google tag (gtag.js) */}
         <Script 
           async 
@@ -115,6 +128,17 @@ export default async function RootLayout({
         />
       </head>
       <body className={font.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WZC5VTKW"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Script
           src="//code.tidio.co/ooc6bralufcpukdjj03swqd2tnk19w9k.js"
           async
